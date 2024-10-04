f9d6211ce789a149ec82c82134fc1b81a54f8715
Feby07 <rahmawatifeby5@gmail.com>
2024-10-04 10:17:11 +0700

    commit

--- Pemilihan2Percobaan1Modifikasi107.java
+++ Pemilihan2Percobaan1Modifikasi107.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan107 {
+public class Pemilihan2Percobaan1Modifikasi107 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
